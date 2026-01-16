4bc3f000602df9a0565b558395c9f086f6da1a65
kemaltt <ttnckemal@gmail.com>
2026-01-16 22:21:13 +0100

    feat: Configure email transporter with host, port, and secure options from environment variables.

--- server/src/services/email.ts
+++ server/src/services/email.ts
@@ -2,10 +2,9 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
-  // host: process.env.EMAIL_HOST,
-  // port: Number(process.env.EMAIL_PORT),
-  // secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
-  service: "gmail",
+  host: process.env.EMAIL_HOST || "smtp.gmail.com",
+  port: Number(process.env.EMAIL_PORT) || 587,
+  secure: process.env.EMAIL_SECURE === "true", // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
