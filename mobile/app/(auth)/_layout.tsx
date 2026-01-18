5bffc9a0d79e7c1bb09da61d1ffbf1e615e41cb1
kemaltt <ttnckemal@gmail.com>
2026-01-18 01:22:39 +0100

    feat: Add forgot-password screen to the authentication flow.

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -5,6 +5,7 @@ export default function AuthLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="register" options={{ headerShown: false }} />
+      <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
     </Stack>
   );
 }
